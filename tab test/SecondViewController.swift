eae1a648da164cddc85edac1917d4915437099c8
Macbook Pro <MacbookPro@as-macbook-pro-2.local>
2017-11-29 21:30:52 +0800

    Initial Commit

---
+++ tab test/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tab test
+//
+//  Created by Macbook Pro on 29/11/2017.
+//  Copyright © 2017 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tab testTests/tab_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  tab_testTests.swift
+//  tab testTests
+//
+//  Created by Macbook Pro on 29/11/2017.
+//  Copyright © 2017 Macbook Pro. All rights reserved.
+//
+
+import XCTest
+@testable import tab_test
+
+class tab_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
