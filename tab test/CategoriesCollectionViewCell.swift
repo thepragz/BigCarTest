d837878ce427e4263942558f29b09128dbafb594
Macbook Pro <MacbookPro@as-MacBook-Pro-2.local>
2017-12-14 21:33:45 +0800

    added categories UI

---
+++ tab test/CategoriesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  tab test
+//
+//  Created by Macbook Pro on 14/12/2017.
+//  Copyright © 2017 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+}
