e8a8390f3578de17b0ac5bb63351b0309d34ddfb
Macbook Pro <MacbookPro@as-MacBook-Pro-2.local>
2017-12-21 20:27:06 +0800

    added date picker page

---
+++ tab test/ExploreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExploreTableViewCell.swift
+//  
+//
+//  Created by Macbook Pro on 20/12/2017.
+//
+
+import UIKit
+
+class ExploreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
