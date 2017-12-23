babf2693bb5e464b15f17490b03661aba71e1a07
Macbook Pro <MacbookPro@as-MacBook-Pro-2.local>
2017-12-23 14:02:59 +0800

    modified ui for tour details page

---
+++ tab test/TripsInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripsInfoTableViewCell.swift
+//  tab test
+//
+//  Created by Macbook Pro on 23/12/2017.
+//  Copyright © 2017 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class TripsInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
