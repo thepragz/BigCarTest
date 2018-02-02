86d5ad95a31d47f2768ce139be2140f6c972b51f
Macbook Pro <MacbookPro@as-MacBook-Pro-2.local>
2018-02-02 20:53:30 +0800

    updated inbox

---
+++ tab test/TripsPurchased/tripsPurcasedCell/TripsCell1TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TripsCell1TableViewCell.swift
+//  tab test
+//
+//  Created by Macbook Pro on 01/02/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class TripsCell1TableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
