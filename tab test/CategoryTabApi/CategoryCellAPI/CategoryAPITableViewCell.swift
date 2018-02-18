05c642d2fff254278744d2dd5c101cb8cdd4c3c4
Macbook Pro <MacbookPro@as-MacBook-Pro-2.local>
2018-02-18 20:46:30 +0800

    added api on Category Tab

---
+++ tab test/CategoryTabApi/CategoryCellAPI/CategoryAPITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryAPITableViewCell.swift
+//  tab test
+//
+//  Created by Macbook Pro on 18/02/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class CategoryAPITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
